76368552a2424735870f366d4d268d86ff579894
JustMax7CB <maximsh4@ac.sce.ac.il>
2021-06-03 23:02:55 +0300

    update

---
+++ setup.py
@@ -0,0 +1,27 @@
+import sys
+
+from cx_Freeze import setup, Executable
+
+includefiles = ['Doctor.xlsx']
+includes = []
+excludes = ["Diagnosis"]
+packages = ['tkinter', 'openpyxl']
+build_exe_options = {'includes': includes, 'packages': packages, 'excludes': excludes, 'include_files': includefiles}
+
+base = None
+if sys.platform == 'win64':
+    base = 'Win64GUI'
+elif sys.platform == 'win32':
+    base = 'Win32GUI'
+
+exe = Executable(
+    script="Main.py",
+    target_name="AutoDoctor",
+    base=base)
+
+setup(
+    name="AutoDoctor",
+    version="1.0",
+    description="Program to diagnose blood results and generate a matching treatment.",
+    options={'build_exe': build_exe_options},
+    executables=[exe],)
